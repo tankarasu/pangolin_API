eda4ab8579a180fb0d88d4ed857d7db159d8ea62
TanKARASU <karasutan@gmail.com>
2021-12-09 10:20:52 +0100

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: { "^#src/(.*)$": "<rootDir>/src/$1" },
+  setupFiles: ["dotenv/config"],
+  setupFilesAfterEnv: ["./jest.setup.js"],
+  collectCoverage: true,
+  collectCoverageFrom: ["**/src/**/*.ts"],
+  testMatch: ["**/test/**/*.spec.ts"]
+};
